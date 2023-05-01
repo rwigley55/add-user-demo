44806e9ac37c4cb88a94f7e9ee205e722baee584
Reed Wigley <rwigley55@gmail.com>
2023-05-01 15:01:35 -0700

    Add ErrorModal component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
+
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
@@ -10,14 +12,14 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
-        //A blank return statment will stop the rest of the function from execution
-        return;
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      //A blank return statment will stop the rest of the function from execution
+      return;
     }
     //Should work, but you are comparing a string to a number (useState initialized the input to a string)
     //the + converts the string back to a number
-    if(+enteredAge < 1) {
-        return;
+    if (+enteredAge < 1) {
+      return;
     }
     // console.log(enteredUsername, enteredAge);
     // Forwarding the enteredUsername and enteredAge to the App component (where the AddUser componenet is being used)
@@ -35,27 +37,32 @@ const AddUser = (props) => {
   };
 
   return (
-    // By default, our custom component doesn't know what to do with "className" prop. Only react built default HTML components can accept this by default
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        {/* "htmlFor" is the HTML prop equivalent for "for" on label accessibility */}
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={enteredUsername}
-          onChange={usernameChangeHandler}
-        />
-        <label htmlFor="age">Age (Years)</label>
-        <input
-          id="age"
-          type="number"
-          value={enteredAge}
-          onChange={ageChangeHandler}
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <div>
+      <ErrorModal title="An error occured" message="Something went wrong" />
+      {/* By default, our custom component doesn't know what to do with
+      "className" prop. Only react built default HTML components can accept this
+      by default */}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          {/* "htmlFor" is the HTML prop equivalent for "for" on label accessibility */}
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
